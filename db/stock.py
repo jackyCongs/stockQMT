af7f1eb5beaac0732b5ffe992a20c9f4d0e1f3cf
congbaochang <congbaochang@ymt360.com>
2024-11-27 20:27:29 +0800

    配置mysql线程池

---
+++ db/stock.py
@@ -0,0 +1,41 @@
+import mysql.connector
+import json
+
+
+def get_stock_list(db_instance):
+    # 连接到数据库
+    conn = db_instance.get_connection()
+    cursor = conn.cursor()
+    row_dict_list = []
+    try:
+        query = "SELECT * FROM stock where is_etf = 1 and inner_etf_type = 'lof' order by id desc"
+        #query = "SELECT * FROM stock where is_etf = 1 order by id desc"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        column_names = [description[0] for description in cursor.description]
+        for row in rows:
+            row_dict_list.append(dict(zip(column_names, row)))
+        return row_dict_list
+    except Exception as e:
+        print(e)
+        return None
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def get_stock_by_code(db_instance, code):
+    conn = db_instance.get_connection()
+    cursor = conn.cursor()
+    try:
+        query = "SELECT * FROM stock where code = %s and is_etf = 1 order by id desc"
+        cursor.execute(query, (str(code),))
+        row = cursor.fetchone()
+        column_names = [description[0] for description in cursor.description]
+        return dict(zip(column_names, row))
+    except Exception as e:
+        print(e)
+        return None
+    finally:
+        cursor.close()
+        conn.close()
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 # coding=utf-8
+
 from xtquant import xtdata
+from db.db_pool import DBPool
+
 
 if __name__ == '__main__':
-    shanghai_funds = xtdata.get_stock_list_in_sector("沪深基金")
-    print(shanghai_funds)
+    db_instance = DBPool()
+    try:
+        db_instance.initialize_pool()
+
+        shanghai_funds = xtdata.get_stock_list_in_sector("沪深基金")
+        print(shanghai_funds)
+    except Exception as e:
+        print(e)
+    finally:
+        # 释放线程池
+        db_instance.close()
