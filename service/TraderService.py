3348340a5d30c1f6346ff25476eb6332371d5bf3
congbaochang <congbaochang@ymt360.com>
2024-12-02 19:57:41 +0800

    remove close

--- service/TraderService.py
+++ service/TraderService.py
@@ -1,10 +1,72 @@
 # coding=utf-8
 
-from xtquant.xttrader import XtQuantTrader
+from xtquant.xttrader import XtQuantTrader, XtQuantTraderCallback
 from xtquant import xtconstant
-from service import account, TradeCallback
+from service import account
 import time
 import threading
+
+
+class TradeCallback(XtQuantTraderCallback):
+    def on_disconnected(self):
+        """
+        连接断开
+        :return:
+        """
+        print("connection lost")
+
+    def on_stock_order(self, order):
+        """
+        委托回报推送
+        :param order: XtOrder对象
+        :return:
+        """
+        print("on order callback:")
+        print(order.stock_code, order.order_status, order.order_sysid)
+
+    def on_stock_trade(self, trade):
+        """
+        成交变动推送
+        :param trade: XtTrade对象
+        :return:
+        """
+        print("on trade callback")
+        print(trade.account_id, trade.stock_code, trade.order_id)
+
+    def on_order_error(self, order_error):
+        """
+        委托失败推送
+        :param order_error:XtOrderError 对象
+        :return:
+        """
+        print("on order_error callback")
+        print(order_error.order_id, order_error.error_id, order_error.error_msg)
+
+    def on_cancel_error(self, cancel_error):
+        """
+        撤单失败推送
+        :param cancel_error: XtCancelError 对象
+        :return:
+        """
+        print("on cancel_error callback")
+        print(cancel_error.order_id, cancel_error.error_id, cancel_error.error_msg)
+
+    def on_order_stock_async_response(self, response):
+        """
+        异步下单回报推送
+        :param response: XtOrderResponse 对象
+        :return:
+        """
+        print("on_order_stock_async_response")
+        print(response.account_id, response.order_id, response.seq)
+
+    def on_account_status(self, status):
+        """
+        :param response: XtAccountStatus 对象
+        :return:
+        """
+        print("on_account_status")
+        print(status.account_id, status.account_type, status.status)
 
 
 class TraderService:
@@ -29,7 +91,7 @@ class TraderService:
     def _connect(self):
         # 建立交易连接
         while True:
-            self.xt_trader.register_callback(TradeCallback.TradeCallback)
+            self.xt_trader.register_callback(TradeCallback)
             self.xt_trader.start()
             connect_result = self.xt_trader.connect()
             if connect_result == 0:
@@ -48,7 +110,6 @@ class TraderService:
             else:
                 print("account trading subscribed FAILED!!! retrying.......")
                 time.sleep(3)
-
 
     # 异步下单
     def async_buy(self, stock_code, bid_price, bid_num, strategy_name, remark, inner_stock_infos):
