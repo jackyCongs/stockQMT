53d42ba036b87d4192d01727891f7fbc7f5caa02
congbaochang <congbaochang@ymt360.com>
2024-12-18 11:32:08 +0800

    增加extra指数订阅方式

--- helper/data_loader.py
+++ helper/data_loader.py
@@ -152,8 +152,8 @@ def get_premium(increase_rate, base_premium_threshold):
     if increase_rate <= 0:
         return Decimal(base_premium_threshold)
     elif increase_rate <= 2:
-        return Decimal(base_premium_threshold) + Decimal(increase_rate / Decimal(4))
-    return Decimal(increase_rate / Decimal(1.5))
+        return Decimal(base_premium_threshold) + Decimal(increase_rate / Decimal(5))
+    return Decimal(increase_rate / Decimal(3))
 
 
 def print_top_variance(inner_stock_infos):
