a07e87d0cc6f61f710683f90119b3dc819a87f99
PC <1242006911@qq.com>
2025-07-17 05:25:43 +0800

    休息的时候不做决策

--- strategies/Strategy1.py
+++ strategies/Strategy1.py
@@ -144,7 +144,7 @@ class Strategy1:
         index_info = self.target_index_infos[stock_info['target_index']]
         # 来自链接第三方订阅的指数，如果更新时间超过5秒就不处理了
         if 'index_updated_time' in index_info:
-            if time.time() - index_info['index_updated_time'] >= 3:
+            if time.time() - index_info['index_updated_time'] >= 2:
                 self.sell_queue.remove_stock(stock_code)
                 self.buy_queue.remove_stock(stock_code)
                 return
@@ -167,7 +167,7 @@ class Strategy1:
         # return
 
         # 9点35以后开始执行
-        if not utils.is_market_after_35():
+        if not utils.is_market_after_35() or utils.is_market_closing() == True:
             return
         # 买一队列中，只有premium大于0就会立即被卖，所以只需要取队列头的第一个数据
         first_buy_queue_node = self.buy_queue.head
